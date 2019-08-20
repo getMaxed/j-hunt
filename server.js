b67c8caa23402e0354f2b6191e8dedb9923c7883
getMaxed <maxfaden07@gmail.com>
2019-08-20 08:50:30 -0700

    init

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+(async () => {
+    await connectDB();
+})();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`server started`));
+
+module.exports = app;
